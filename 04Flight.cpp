1687f6cb429104a56987de6b2dad1405528227c3
Samrudhi Mahendra Borawake <157286379+Samrudhi24Code@users.noreply.github.com>
2024-05-02 10:05:51 +0530

    Update and rename Flight.cpp to 04Flight.cpp

--- 04Flight.cpp
+++ 04Flight.cpp
@@ -1,4 +1,4 @@
-/* Group C Assignment 14 : Graph DS representation using Adjacency list for Flight path between cities program
+/* Group C Assignment 4 : Graph DS representation using Adjacency list for Flight path between cities program
 on June 26, 2021
  Problem statement :
 There are flight paths between cities. If there is a flight between city A and city B then there is an edge between the cities. The cost of the edge can be the time that flight take to reach city B from A, or the amount of fuel used for the journey. Represent this as a graph. The node can be represented by airport name or name of the city. Use adjacency list representation of the graph or use adjacency matrix representation of the graph.
